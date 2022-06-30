7505eec7f28687a22a99825f884f3c09a941eec1
Aniket Sudke <97212869+Aniketsudke@users.noreply.github.com>
2022-07-01 00:01:49 +0530

    Add files via upload

---
+++ lecture1.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main(){
+    int aniket = 10;
+    int Aniket = 15;
+    printf("%d\n",aniket);
+    printf("%d",Aniket);
+
+    // Single Line Comment ---> Don't Delate this below line 
+    // Multiple Line Comment
+    /*My name is Aniket 
+    Please Subscribe my channel
+    */
+    return 0;
+}
---
+++ lecture2.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(){
+    // ********Input/Output*******
+    // printf("Hello World");
+    // int q;
+    // printf("Enter the value of q:");
+    // scanf("%d",&q);
+    // printf("Value of q is %d",q);
+    // ********Instruction*******
+    // int i = 14;
+    // int j = i;
+    // printf("J = %d\n",j);
+    // printf("I = %d",i);
+    // int x = 45;
+    // int z = x + 1;
+    // printf("%d",z);
+    int z = 10%3;
+    printf("%d",z);
+    return 0;
+}
